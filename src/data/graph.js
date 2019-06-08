6a5a95cfb9d9f1695e9f8941c625b1d3b9d67b3f
lolimay <lolimay@lolimay.cn>
2019-06-08 16:19:27 +0800

    Update favicon

---
+++ src/data/graph.js
@@ -0,0 +1,11 @@
+export default [
+	{
+		_id: 'thefirstone',
+		title: '百度一下，你就知道',
+		faviconUrl: 'https://www.baidu.com/favicon.ico',
+		currentUrl: 'https://www.baidu.com',
+		origin: null,
+		timestamp: '1559981132834',
+		count: 5
+	}
+]
