85dd54b9cc0b062c5f2e90b6404839d041645ec9
lolimay <lolimay@lolimay.cn>
2019-06-09 04:48:08 +0800

    Co-authored-by: nuobta <50618377+nuobta@users.noreply.github.com>

--- src/container/BrowsingActivities.js
+++ src/container/BrowsingActivities.js
@@ -6,58 +6,62 @@ class FocusGraph extends React.Component {
 	static NODE_R = 8;
 
 	state = {
-	  data: this.props.data,
-	  highlightNodes: [],
-	  highlightLink: null,
-	  isHover: false
+		data: this.props.data,
+		highlightNodes: [],
+		highlightLink: null,
+		isHover: false
 	};
 
 	_handleClick = node => {
 		// Aim at node from outside it
 		const distance = 40;
-		const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+		const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
 		this.fg.cameraPosition(
-		  { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
-		  node, // lookAt ({ x, y, z })
-		  3000  // ms transition duration
+			{ x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+			node, // lookAt ({ x, y, z })
+			3000  // ms transition duration
 		);
-	  };
+	};
 
 	_handleNodeHover = node => {
-	  this.setState({ highlightNodes: node ? [node] : [] });
+		this.setState({ highlightNodes: node ? [node] : [] });
 	};
 
 	_handleLinkHover = link => {
-	  this.setState({
-		highlightLink: link,
-		highlightNodes: link ? [link.source, link.target] : []
-	  });
+		this.setState({
+			highlightLink: link,
+			highlightNodes: link ? [link.source, link.target] : []
+		});
 	};
 
 	_paintRing = (node, ctx) => {
-	  const { NODE_R } = FocusGraph;
+		const { NODE_R } = FocusGraph;
 
-	  ctx.beginPath();
-	  ctx.arc(node.x, node.y, NODE_R * 1.4, 0, 2 * Math.PI, false);
-	  ctx.fillStyle = 'red';
-	  ctx.fill();
+		ctx.beginPath();
+		ctx.arc(node.x, node.y, NODE_R * 1.4, 0, 2 * Math.PI, false);
+		ctx.fillStyle = 'red';
+		ctx.fill();
 	};
-	  
+
 	render() {
-	  const { data, highlightNodes, highlightLink } = this.state;
-	  return <ForceGraph2D
-		ref={el => { this.fg = el; }}
-		graphData={data}
-		linkWidth={link => link === highlightLink ? 5 : 1}
-		linkDirectionalParticles={4}
-		linkDirectionalParticleWidth={link => link === highlightLink ? 4 : 0}
-		nodeCanvasObjectMode={node => highlightNodes.indexOf(node) !== -1 ? 'before' : undefined}
-		nodeCanvasObject={this._paintRing}
-		onNodeHover={this._handleNodeHover}
-		onLinkHover={this._handleLinkHover}
-		onNodeClick={this._handleClick}
-	  />;
+		const { data, highlightNodes, highlightLink } = this.state;
+		return <ForceGraph2D style={style}
+			ref={el => { this.fg = el; }}
+			graphData={data}
+			linkWidth={link => link === highlightLink ? 5 : 1}
+			linkDirectionalParticles={4}
+			linkDirectionalParticleWidth={link => link === highlightLink ? 4 : 0}
+			nodeCanvasObjectMode={node => highlightNodes.indexOf(node) !== -1 ? 'before' : undefined}
+			nodeCanvasObject={this._paintRing}
+			onNodeHover={this._handleNodeHover}
+			onLinkHover={this._handleLinkHover}
+			onNodeClick={this._handleClick}
+		/>;
 	}
-  }
+}
+
+const style = {
+	width: 439
+}
 
 export default FocusGraph;
--- src/container/dashboard/Chart.js
+++ src/container/dashboard/Chart.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Title from './Title';
 import BrowsingActivities from '../BrowsingActivities';
 import mockData from '../../data/graph';
 
 export default function Chart() {
   return (
     <React.Fragment>
-      <Title>我的足迹</Title>
       <BrowsingActivities data={ mockData }/>
     </React.Fragment>
   );
