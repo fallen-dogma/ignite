08d0905fbc0164d1415b7dcc70d9d3206b585281
lolimay <lolimay@lolimay.cn>
2019-06-09 02:56:27 +0800

    [NEW] Add TimeMaganement

--- src/container/dashboard/Deposits.js
+++ src/container/dashboard/Deposits.js
@@ -1,33 +1,25 @@
-/* eslint-disable no-script-url */
-
 import React from 'react';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import Title from './Title';
+import ReactEcharts from 'echarts-for-react';
+import timeChartOption from '../../data/timeChartOptions';
+import echarts from 'echarts';
+import echartsTheme from '../../lib/echartTheme.json';
 
-const useStyles = makeStyles({
-  depositContext: {
-    flex: 1,
-  },
-});
+export default class Deposits extends React.Component {
+  componentWillMount() {
+    echarts.registerTheme('custom', echartsTheme);
+  }
 
-export default function Deposits() {
-  const classes = useStyles();
-  return (
-    <React.Fragment>
-      <Title>Recent Deposits</Title>
-      <Typography component="p" variant="h4">
-        $3,024.00
-      </Typography>
-      <Typography color="textSecondary" className={classes.depositContext}>
-        on 15 March, 2019
-      </Typography>
-      <div>
-        <Link color="primary" href="javascript:;">
-          View balance
-        </Link>
-      </div>
-    </React.Fragment>
-  );
+  render() {
+    return (
+      <React.Fragment>
+        <Title>时间管理</Title>
+        <ReactEcharts
+        option={timeChartOption}
+        notMerge={true}
+        lazyUpdate={true}
+        theme={'custom'} />
+      </React.Fragment>
+    );
+  }
 }
