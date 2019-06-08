71847f9da4d7d80f0d43494403a3a46a376ea331
lolimay <lolimay@lolimay.cn>
2019-06-08 23:15:56 +0800

    Update AppBar.js

--- src/container/AppBar.js
+++ src/container/AppBar.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { default as AppConfig } from '../constants/App';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -30,7 +31,7 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            Ignite
+            { AppConfig.PROJECT_NAME }
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
--- src/container/dashboard/Dashboard.js
+++ src/container/dashboard/Dashboard.js
@@ -146,7 +146,7 @@ export default function Dashboard() {
             <MenuIcon />
           </IconButton>
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            Dashboard
+            { AppConfig.PROJECT_NAME }
           </Typography>
           <IconButton color="inherit">
             <Badge badgeContent={4} color="secondary">
